3a6022837e39c3b646742eb92230f3f58cf8eb0b
MASTERMIND237 <essonguemarcien25@gmail.com>
2026-05-18 10:51:39 +0100

    parametrage du cors cote frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  baseURL: 'http://localhost:8000/api/satisfy',
+  baseURL: 'https://backend-laravel-lbsv.onrender.com',
    withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
